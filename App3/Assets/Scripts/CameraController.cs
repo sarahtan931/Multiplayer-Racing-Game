8c8bb0869d7fb4f34b11cd6e899d64e89f8a35cf
sarahtan931 <stan229@uwo.ca>
2022-11-22 12:48:15 -0500

    fixing camera follow

--- App3/Assets/Scripts/CameraController.cs
+++ App3/Assets/Scripts/CameraController.cs
@@ -1,24 +1,29 @@
 using UnityEngine;
 using System.Collections;
+using Unity.VisualScripting;
 
 public class CameraController : MonoBehaviour
 {
 
     public GameObject player;      
     private Vector3 offset;            //Private variable to store the offset distance between the player and camera
+    private Vector3 _InitialCamPos;
 
     // Use this for initialization
     void Start()
     {
         //Calculate and store the offset value by getting the distance between the player's position and camera's position.
         offset = transform.position - player.transform.position;
+        _InitialCamPos = transform.InverseTransformPoint(transform.position);
+
     }
 
     // LateUpdate is called after Update each frame
     void LateUpdate()
     {
-        // Set the position of the camera's transform to be the same as the player's, but offset by the calculated offset distance.
-        transform.position = player.transform.position + new Vector3(0, 5, -7);
-        //transform.rotation = player.transform.rotation;
+        Vector3 back = -player.transform.forward;
+        back.y = 0.5f; // this determines how high. Increase for higher view angle.
+        transform.position = player.transform.position - back * -5;
+        transform.forward = player.transform.position - transform.position;
     }
 }
