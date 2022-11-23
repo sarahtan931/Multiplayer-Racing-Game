4e83ac2fd1220e9fffc1026e1a720fc61a8cabd6
sarahtan931 <stan229@uwo.ca>
2022-11-22 20:23:25 -0500

    adding color gradient to health bar

--- App3/Assets/Scripts/HealthBar.cs
+++ App3/Assets/Scripts/HealthBar.cs
@@ -6,15 +6,19 @@ using UnityEngine.UI;
 public class HealthBar : MonoBehaviour
 {
     public Slider slider;
+    public Gradient gradient;
+    public Image fill;
 
     public void SetHealth(int health)
     {
         slider.value = health;
+        fill.color = gradient.Evaluate(slider.normalizedValue);
     }
 
     public void SetMaxHealth(int health)
     {
         slider.maxValue = health;
         slider.value = health;
+        fill.color = gradient.Evaluate(1f);
     }
 }
