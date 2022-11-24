a3b22cdc07cacb8268ea85c382cdde49316f1924
nushsamy <asamara5@uwo.ca>
2022-11-24 10:40:41 -0500

    added timer

---
+++ App3/Assets/Scripts/TimerScript.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class TimerScript : MonoBehaviour
+{
+
+    public static TimerScript instance = null;
+    public static float timeRemaining = 180;
+    public static bool timerIsRunning = true;
+
+    void Start()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(this.gameObject);
+        }
+        else
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (timerIsRunning)
+        {
+            if (timeRemaining > 0)
+            {
+                timeRemaining -= Time.deltaTime;
+                DisplayTime(timeRemaining);
+
+            }
+            else
+            {
+                Debug.Log("Time has run out!");
+                timeRemaining = 0;
+                timerIsRunning = false;
+                SceneManager.LoadScene("OnePlayerLeaderboard");
+            }
+        }
+    }
+
+    void DisplayTime(float timeToDisplay)
+    {
+        TMPro.TMP_Text timeText = GameObject.Find("TimerText").GetComponent<TMPro.TMP_Text>();
+        if (timeText.text != null)
+        {
+            float minutes = Mathf.FloorToInt(timeRemaining / 60);
+            float seconds = Mathf.FloorToInt(timeRemaining % 60);
+            timeText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+        }
+
+    }
+}
